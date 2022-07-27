7886c9073f0ba23e22d07dd6790cba165680adbf
naufalzm30 <naufalzm30@gmail.com>
2022-07-28 00:12:50 +0700

    add graph windrose bar, contour, also graph per second and minute

---
+++ convert-csv/cek.py
@@ -0,0 +1,7 @@
+a='00:00:00'
+b='00:00:01'
+
+if a<b:
+    print("ya")
+else:
+    print("no")
--- convert-csv/convert.py
+++ convert-csv/convert.py
@@ -19,7 +19,8 @@ import pandas as pd
 import matplotlib.cm as cm
 from math import pi
 from windrose import WindroseAxes
-from tqdm import tqdm_gui
+from tqdm import tqdm
+import xlsxwriter
 
 
 start_time = time.time()
@@ -29,11 +30,6 @@ start_time = time.time()
 #104c84
 
 
-# window= Tk()
-# button = Button(text="open file",command=openFile)
-
-# button.pack()
-# window.mainloop()
 def main():
     root_progress = Tk()
     progress  = ttk.Progressbar(root_progress, orient = HORIZONTAL, length=180)
@@ -60,14 +56,7 @@ def main():
             b.append(i["Kec_angin"])
         pool_grafik_detik_a.append(a)
         pool_grafik_detik_b.append(b)
-        # plt.figure(jcounter+1)
-        # plt.plot(a, b)
-        # ax1 = plt.figure("per_second_{}".format(jcounter+1),figsize=(10, 3)).add_subplot(111)
-        # ax1.plot(a, b) 
-        # # start, end = ax1.get_xlim()
-        # ax1.set_xticks(a[::10800])
-        # ax1.set_ylabel("Kec angin m/s")
-        # plt.close(plt.figure("per_second_{}".format(jcounter+1),figsize=(10, 3)))
+      
         json_untuk_csv = proses_per_menit(json_raw_second)
         # print(json_untuk_csv)
         c=[]
@@ -123,7 +112,7 @@ def main():
                     barat_laut.append(param_arah_angin)
                     data_windrose.append(param_arah_angin)
                 
-
+        #JANGAN DI HAPUS INI BUAT DAPETIN MIN MAX
         # get data max
         # max_attr = max(barat_daya, key=lambda x: x['Kec_angin'])
         # min_attr = min(barat_daya, key=lambda x: x['Kec_angin'])
@@ -132,24 +121,12 @@ def main():
         # print(min_attr)
 
 
-
-         
-        # df['Arah_angin']=df['Arah_angin'].astype('string')
-        # print(df['Arah_angin'])
-        # wx = WindroseAxes.from_ax()
-        # wx.bar(df.Arah_angin, df.Kec_angin, normed=True, opening=0.8, edgecolor='white')
-        # wx.set_legend()
         pool_data_windrose.append(data_windrose)
         
         pool_grafik_menit_c.append(c)
         pool_grafik_menit_d.append(d)
 
-        # ax2 = plt.figure("per_menit_{}".format(jcounter+1),figsize=(10, 3)).add_subplot(111)
-        # ax2.plot(c, d) 
-        # # start, end = ax1.get_xlim()
-        # ax2.set_xticks(c[::241])
-        # ax2.set_ylabel("Kec angin m/s")
-        # plt.close(plt.figure("per_menit_{}".format(jcounter+1),figsize=(10, 3)))
+
 
         pool_json_raw.append(json_raw_second)
         pool_json_csv.append(json_untuk_csv)
@@ -166,35 +143,122 @@ def main():
     print(pool_path_simpan)
     kcounter=0
     nama_file_simpan=[]
-    for k in (file_json_raw):
+    for k in tqdm(file_json_raw):
         
         file_name = os.path.basename(k).split('.')[0]
         file_name_custom=file_name.split('-')
 
-        simpan_csv("WRA(P)-22-{}-{}".format(file_name_custom[1],file_name_custom[2]),pool_json_csv[kcounter],pool_path_simpan)
-        simpan_csv("WRA-22-{}-{}".format(file_name_custom[1],file_name_custom[2]),pool_json_raw[kcounter],pool_path_simpan)
-        simpan_json("WRA(P)-22-{}-{}".format(file_name_custom[1],file_name_custom[2]),(json.dumps(pool_json_csv[kcounter])),pool_path_simpan)
-        nama_file_simpan.append("WRA-22-{}-{}".format(file_name_custom[1],file_name_custom[2]))
-        # plt.figure(kcounter+1)
-        # plt.savefig("{}\Line_Chart_Second_{}".format(pool_path_simpan,kcounter))
+        simpan_csv("WRA(P)-22-{}-{}".format(file_name_custom[2],file_name_custom[3]),pool_json_csv[kcounter],pool_path_simpan)
+        simpan_csv("WRA-22-{}-{}".format(file_name_custom[2],file_name_custom[3]),pool_json_raw[kcounter],pool_path_simpan)
+        simpan_json("WRA(P)-22-{}-{}".format(file_name_custom[2],file_name_custom[3]),(json.dumps(pool_json_csv[kcounter])),pool_path_simpan)
+        nama_file_simpan.append("WRA-22-{}-{}".format(file_name_custom[2],file_name_custom[3]))
+            
 
-        # plt.figure("per_second_{}".format(kcounter+1),figsize=(10, 3)).set_canvas(new_manager.canvas)        
-        # plt.savefig("{}\Grafik per detik WRA-22-{}-{}.png".format(pool_path_simpan,file_name_custom[1],file_name_custom[2]))
-        # plt.close(plt.figure("per_second_{}".format(kcounter+1),figsize=(10, 3)))
 
-        # plt.figure("per_menit_{}".format(kcounter+1),figsize=(10, 3)).set_canvas(new_manager2.canvas) 
-        # plt.savefig("{}\Grafik per menit WRA-22-{}-{}.png".format(pool_path_simpan,file_name_custom[1],file_name_custom[2]))
-        # plt.close(plt.figure("per_menit_{}".format(kcounter+1),figsize=(10, 3)))
-
+        # Create a Pandas dataframe from some data.
+        # df = pd.DataFrame({'Time':pool_grafik_detik_a[kcounter],'Kec_angin':pool_grafik_detik_b[kcounter]})
         
-        # savefig() supports eps, jpeg, jpg, pdf, pgf, png, ps, raw, rgba, svg, svgz, tif, tiff
+        # df['Time']= df['Time'].astype('string')
+
+
+        # df_m = pd.DataFrame({'Time':pool_grafik_menit_c[kcounter],'Kec_angin':pool_grafik_menit_d[kcounter]})
         
+        # df_m['Time']= df_m['Time'].astype('string')
+
+        # Create a Pandas Excel writer using XlsxWriter as the engine.
+        # writer = pd.ExcelWriter('{}\dashboard WRA-22-{}-{}.xlsx'.format(pool_path_simpan,file_name_custom[2],file_name_custom[3]), engine='xlsxwriter')
+
+        workbook = xlsxwriter.Workbook('{}\WRA(A)-22-{}-{}.xlsx'.format(pool_path_simpan,file_name_custom[2],file_name_custom[3]))
+        worksheet_1 = workbook.add_worksheet('per_detik')
+        worksheet_2 = workbook.add_worksheet('per_menit')
+
+        # time_format = workbook.add_format({'num_format': 'h:mm:ss'})
+        worksheet_1.write('A1', 'Time')
+        worksheet_1.write('B1', 'Kec_angin')
+        worksheet_2.write('A1', 'Time')
+        worksheet_2.write('B1', 'Kec_angin')
+
+        row = 1
+        col = 0
+
+        for time_detik in (pool_grafik_detik_a[kcounter]):
+            # Convert the date string into a datetime object.
+            # time_strp = datetime.strptime(time_detik, "%H:%M:%S")
+
+            worksheet_1.write(row, col, time_detik)
+            worksheet_1.write(row, col+1, pool_grafik_detik_b[kcounter][row-1] )
+
+            if (time_detik==pool_grafik_detik_a[kcounter][-1]):
+                row=1
+            else:
+                row += 1
+        print(row)
+        for time_menit in (pool_grafik_menit_c[kcounter]):
+            # Convert the date string into a datetime object.
+            # time_strp = datetime.strptime(time_menit, "%H:%M:%S")
+
+            worksheet_2.write(row, col, time_menit)
+            worksheet_2.write(row, col+1, pool_grafik_menit_d[kcounter][row-1] )
+
+            if (time_menit==pool_grafik_menit_c[kcounter][-1]):
+                row=1
+            else:
+                row += 1
+        # worksheet_1.set_column('A:A', None, time_format) 
+        # worksheet_2.set_column('A:A', None, time_format) 
+        # Convert the dataframe to an XlsxWriter Excel object.
+        # df.to_excel(writer, sheet_name='per_detik',index=False)
+
+        # df_m.to_excel(writer, sheet_name='per_menit',index=False)
+
+        # Get the xlsxwriter workbook and worksheet objects.
+        # workbook  = writer.book
+
+        # my_format = workbook.add_format()
+        # my_format.set_num_format('hh:mm:ss')
+        # worksheet_1 = writer.sheets['per_detik']
+        # worksheet_2 = writer.sheets['per_menit']
+        # worksheet_1.set_column('A:A', None, my_format)
+        # worksheet_2.set_column('A:A', None, my_format)
+
+        # Create a chart object.
+        chart = workbook.add_chart({'type': 'line'})
+        chart_menit=workbook.add_chart({'type': 'line'})
+
+
+        # Get the dimensions of the dataframe.
+        # (max_row, max_col) = df.shape
+
+        # Configure the series of the chart from the dataframe data.
+        chart.add_series({'categories': '=per_detik!$A$1:$A$86401','values':'=per_detik!$B$1:$B$86401'})
+
+        chart.set_legend({'delete_series': [0]})
+
+        chart.set_x_axis({'text_axis': True,'interval_unit': 10801,'num_font':  {'rotation': 45}})
+
+        chart.set_y_axis({'name':'Kecepatan Angin (m/s)'})
+
+        chart_menit.add_series({'categories': '=per_menit!$A$1:$A$1441','values':'=per_menit!$B$1:$B$1441'})
+
+        chart_menit.set_x_axis({'text_axis': True})
+
+        chart_menit.set_y_axis({'name':'Kecepatan Angin (m/s)'})
+
+        chart_menit.set_legend({'delete_series': [0]})
+        # Insert the chart into the worksheet.
+        worksheet_1.insert_chart(1, 3, chart)
+
+        worksheet_2.insert_chart(1, 3, chart_menit)
+
+        workbook.close()
+
+
         ax1 = plt.figure("per_second_{}".format(kcounter+1),figsize=(10, 3)).add_subplot(111)
         ax1.plot(pool_grafik_detik_a[kcounter], pool_grafik_detik_b[kcounter]) 
         # start, end = ax1.get_xlim()
         ax1.set_xticks(pool_grafik_detik_a[kcounter][::10800])
         ax1.set_ylabel("Kec angin m/s")
-        plt.savefig("{}\Grafik per detik WRA-22-{}-{}.png".format(pool_path_simpan,file_name_custom[1],file_name_custom[2]))
+        plt.savefig("{}\Grafik per detik WRA-22-{}-{}.png".format(pool_path_simpan,file_name_custom[2],file_name_custom[3]))
         plt.close(plt.figure("per_second_{}".format(kcounter+1),figsize=(10, 3)))
 
         ax2 = plt.figure("per_menit_{}".format(kcounter+1),figsize=(10, 3)).add_subplot(111)
@@ -202,7 +266,7 @@ def main():
         # start, end = ax1.get_xlim()
         ax2.set_xticks(pool_grafik_menit_c[kcounter][::241])
         ax2.set_ylabel("Kec angin m/s")
-        plt.savefig("{}\Grafik per menit WRA-22-{}-{}.png".format(pool_path_simpan,file_name_custom[1],file_name_custom[2]))
+        plt.savefig("{}\Grafik per menit WRA-22-{}-{}.png".format(pool_path_simpan,file_name_custom[2],file_name_custom[3]))
         plt.close(plt.figure("per_menit_{}".format(kcounter+1),figsize=(10, 3)))
         
 
@@ -210,22 +274,19 @@ def main():
         wx = WindroseAxes.from_ax()
         wx.contour(df['Derajat_angin'],df['Kec_angin'],bins=np.arange(0, 5), cmap=cm.hot, lw=3)
         wx.set_legend(bbox_to_anchor=[-0.1, 0],loc='lower left')
-        plt.savefig("{}\Windrose WRA-22-{}-{}.png".format(pool_path_simpan,file_name_custom[1],file_name_custom[2]))
+        plt.savefig("{}\Windrose contour WRA-22-{}-{}.png".format(pool_path_simpan,file_name_custom[2],file_name_custom[3]))
+        plt.close()
+
+        wx = WindroseAxes.from_ax()
+        wx.bar(df['Derajat_angin'],df['Kec_angin'],opening=0.8)
+        wx.set_legend(bbox_to_anchor=[-0.1, 0],loc='lower left')
+        plt.savefig("{}\Windrose bar WRA-22-{}-{}.png".format(pool_path_simpan,file_name_custom[2],file_name_custom[3]))
         plt.close()
         
         kcounter+=1
         if(k==file_json_raw[-1]):
             kcounter=0
     
-    # for windrose in pool_data_windrose:
-    #     df = pd.DataFrame(windrose)
-    #     wx = WindroseAxes.from_ax()
-    #     wx.contour(df['Derajat_angin'],df['Kec_angin'],bins=np.arange(0, 5, 4), cmap=cm.hot, lw=3)
-    #     wx.set_legend(bbox_to_anchor=[-0.1, 0],loc='lower left')
-    #     plt.savefig("{}\Windrose {}.png".format(pool_path_simpan,nama_file_simpan[wcounter]))
-    #     plt.close()
-    #     wcounter+=1
-
     print("convert sudah selesai :)")
 
     process = psutil.Process(os.getpid())  
@@ -286,20 +347,6 @@ wpercent = (basewidth / float(img.size[0]))
 hsize = int((float(img.size[1]) * float(wpercent)))
 img = img.resize((basewidth, hsize))
 new_image= ImageTk.PhotoImage(img)
-
-# label_button= Label(w,image=new_image,borderwidth=0)
-# label_button.place(x=475,y=320,anchor='center')
-# photoimage = PhotoImage(file="main.png", width=900)
-# canvas.create_image(500, 200, image=photoimage)
-
-
-# MyLabel = Label(w,text="tes\nhd",font=('Wonderbar',45))
-# MyLabel.configure(bg='#104c84',fg='yellow',pady=100)
-# MyLabel.pack()
-
-# MyLabel1 = Label(w,text="hehe",font=('Wonderbar',25))
-# MyLabel1.configure(bg='#104c84',fg='yellow')
-# MyLabel1.pack()
 
 myButton = Button(w, image=new_image,bd=0,command=main)
 myButton.bind('<Enter>', on_enter)
